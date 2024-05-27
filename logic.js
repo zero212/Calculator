b6eda4a7c29ac51397fc7e1af9576680df06676d
moon8603 <buikimngan2003@gmail.com>
2024-05-27 21:18:25 +0900

    add logic.js

---
+++ logic.js
@@ -0,0 +1,66 @@
+var button1 = document.getElementById('button-1');
+var button2 = document.getElementById('button-2');
+var button3 = document.getElementById('button-3');
+var button4 = document.getElementById('button-4');
+var button5 = document.getElementById('button-5');
+var button6 = document.getElementById('button-6');
+var button7 = document.getElementById('button-7');
+var button8 = document.getElementById('button-8');
+var button9 = document.getElementById('button-9');
+var button0 = document.getElementById('button-0');
+
+// 입력 필드에 대한 참조를 가져옵니다.
+var input = document.querySelector('.value');
+
+// 버튼에 클릭 이벤트 리스너를 추가합니다.
+button1.addEventListener('click', function() {
+  // 버튼의 텍스트 내용을 입력 필드의 값으로 설정합니다.
+  input.value += this.textContent;
+});
+
+button2.addEventListener('click', function() {
+    // 버튼의 텍스트 내용을 입력 필드의 값에 추가합니다.
+    input.value += this.textContent;
+});
+
+button3.addEventListener('click', function() {
+    // 버튼의 텍스트 내용을 입력 필드의 값에 추가합니다.
+    input.value += this.textContent;
+});
+
+button4.addEventListener('click', function() {
+    // 버튼의 텍스트 내용을 입력 필드의 값에 추가합니다.
+    input.value += this.textContent;
+});
+
+button5.addEventListener('click', function() {
+    // 버튼의 텍스트 내용을 입력 필드의 값에 추가합니다.
+    input.value += this.textContent;
+});
+
+button6.addEventListener('click', function() {
+    // 버튼의 텍스트 내용을 입력 필드의 값에 추가합니다.
+    input.value += this.textContent;
+});
+
+button7.addEventListener('click', function() {
+    // 버튼의 텍스트 내용을 입력 필드의 값에 추가합니다.
+    input.value += this.textContent;
+});
+
+button8.addEventListener('click', function() {
+    // 버튼의 텍스트 내용을 입력 필드의 값에 추가합니다.
+    input.value += this.textContent;
+});
+
+button9.addEventListener('click', function() {
+    // 버튼의 텍스트 내용을 입력 필드의 값에 추가합니다.
+    input.value += this.textContent;
+});
+
+button0.addEventListener('click', function() {
+    // 버튼의 텍스트 내용을 입력 필드의 값에 추가합니다.
+    input.value += this.textContent;
+});
+
+
