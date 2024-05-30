5aa36ceec69d5fdcce50a879eb42331815e728bb
Zero <31235907+zero212@users.noreply.github.com>
2024-05-30 17:46:50 +0200

    Add js code

--- logic.js
+++ logic.js
@@ -1,10 +1,68 @@
 let buttonList = document.getElementsByClassName("button");
-let input = document.getElementsByClassName('result')[0];
+let resultField = document.querySelector(".result");
+let reset = false;
 
 for(i = 0; i < buttonList.length; i++) {
     let button = buttonList[i];
     button.addEventListener("click", function(){
-        input.value += button.textContent;
+        if (reset) {
+            resultField.value = button.textContent;
+            reset = false;
+            return;
+        }
+        resultField.value += button.textContent;
     });
 }
+
+let equalButton = document.getElementById("button-equal");
+equalButton.addEventListener("click", function(){
+    let currentValue = resultField.value;
+    if (currentValue.length == 0 ){
+        return;
+    }
+    // replace × with *
+    currentValue = currentValue.replace("×", "*");
+    // replace ÷ with /
+    currentValue = currentValue.replace("÷", "/");
+    let result = eval(currentValue);
+    resultField.value = result;
+    reset = true;
+})
+
+let clearButton = document.getElementById("clear-button");
+clearButton.addEventListener("click", function(){
+    resultField.value = '';
+});
+
+let backButton = document.getElementById("back-button");
+backButton.addEventListener("click", function(){
+    let currentValue = resultField.value;
+    if (currentValue.length <= 0) {
+        return;
+    } 
+    resultField.value = currentValue.substring(0, currentValue.length - 1);
+});
+
+let operatorButtonList = document.getElementsByClassName("button-oprt");
+for (i = 0; i < operatorButtonList.length; i++) {
+    let button = operatorButtonList[i];
+    button.addEventListener("click", function() {
+        let currentValue = resultField.value;
+        if (currentValue.length == 0 && button.textContent != "-") {
+            return;
+        }
+        let lastCharacter = currentValue.charAt(currentValue.length - 1);
+        if (lastCharacter == '+' ||
+        lastCharacter == '-' ||
+        lastCharacter == '×' ||
+        lastCharacter == '÷') {
+           // Do nothing
+        } else {
+            resultField.value += button.textContent;
+            reset = false;
+        }
+    });
+}
+
+
 
